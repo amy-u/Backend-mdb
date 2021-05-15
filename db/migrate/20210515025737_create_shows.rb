f6a439f5d586ed11c8ab8ff5ecf8c79688557994
amy-u <amyung.lm@gmail.com>
2021-05-14 20:31:28 -0700

    Polymorphic Options

---
+++ db/migrate/20210515025737_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[5.0]
+  def change
+    create_table :shows do |t|
+      t.string :title
+      t.integer :episode
+
+      t.timestamps
+    end
+  end
+end
